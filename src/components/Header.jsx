5f0fc401a3ed5fa8906b37cf6a874b138f7931f2
Anna Kuzmenko <84227066+AnnaKuzmenko17@users.noreply.github.com>
2024-03-28 18:03:06 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/investment-calculator-logo.png";
+import logo from "../vite-deploy-demo/assets/investment-calculator-logo.png";
 
 export default function Header () {
   return (
