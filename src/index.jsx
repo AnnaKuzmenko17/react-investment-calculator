4da861fb8fbf0f901a6111056a9992f7918d8b82
ANNA KUZMENKO <anna.kuzmenko120063@gmail.com>
2024-03-29 12:34:15 +0200

    m

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
-import './index.css';
+import './REACT_INVESTMENT_APP/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
