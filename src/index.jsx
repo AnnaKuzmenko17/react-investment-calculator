3418f35daf3d7fa4de0cfc8ce42c76bd39246eef
Anna Kuzmenko <84227066+AnnaKuzmenko17@users.noreply.github.com>
2024-03-29 12:14:08 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
-import '.react-investment-calculator/index.css';
+import './react-investment-calculator/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
